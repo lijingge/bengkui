166340de71edf4ef597e6b5657c10a75d9940657
李敬歌 <2428158682@qq.com>
2019-02-14 20:15:56 +0800

    项目创建

---
+++ src/main/java/com/bengkui/BengkuiApplication.java
@@ -0,0 +1,14 @@
+package com.bengkui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BengkuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BengkuiApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/bengkui/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.bengkui;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BengkuiApplication.class);
+    }
+
+}
+
